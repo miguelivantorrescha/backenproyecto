9255a5c77fba49c057b73fd78e2a5db901792e9d
miguel ivan <mitchy97@gmail.com>
2021-11-14 18:50:37 -0500

    version 1.0.0

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,6 @@
 export * from './ping.controller';
+export * from './pedidos-persona.controller';
+export * from './persona-pedidos.controller';
+export * from './persona.controller';
+export * from './pedido.controller';
+export * from './producto.controller';
